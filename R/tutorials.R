286e95771c624e8763eb69e491650bc298e14aff
rsquaredin <rsquaredin@gmail.com>
2017-09-22 20:56:18 +0530

    modify list_tutorials function

--- R/tutorials.R
+++ R/tutorials.R
@@ -1,11 +1,25 @@
-#' @importFrom tibble tibble
-#' @title List Tutorials
-#' @description List all the tutorials available
+#' @importFrom learnr run_tutorial
+#' @title Display Tutorials
+#' @description Show all tutorials
 #' @export
-#'
-list_tutorials <- function() {
-  out <- tibble(title = c('Learn dplyr', 'Learn Matrix'),
-                description = c('Learn to wrangle data with dplyr',
-                                'Learn matrix'))
-  return(out)
+
+list_tutorials <- function(example) {
+  # locate all the shiny app examples that exist
+  validExamples <- list.files(system.file("tutorials", package = "trainr"))
+
+  validExamplesMsg <-
+    paste0(
+      "The following tutorials are available: \n",
+      paste0("'", validExamples, "'", collapse = "\n"))
+
+  # if an invalid example is given, throw an error
+  if (missing(example) || !nzchar(example) ||
+      !example %in% validExamples) {
+    message(validExamplesMsg)
+  }
+
+  # find and launch the app
+  # appDir <- system.file("tutorials", example, package = "trainr")
+  # shiny::runApp(appDir, display.mode = "normal")
+  # learnr::run_tutorial(name = example, package = 'trainr')
 }
