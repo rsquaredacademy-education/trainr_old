89bf4dcfb37e34d70deef5ebb9d5b8a0449dfc8b
rsquaredin <rsquaredin@gmail.com>
2017-11-24 18:40:01 +0530

    remove argument from function

--- R/tutorials.R
+++ R/tutorials.R
@@ -1,21 +1,19 @@
 #' @importFrom learnr run_tutorial
+#' @importFrom cli rule
+#' @importFrom crayon red blue
 #' @title Display Tutorials
 #' @description Show all tutorials
 #' @export
 
-learn_tutorials_list <- function(example) {
-  # locate all the shiny app examples that exist
+learn_tutorials_list <- function() {
+  # locate all the tutorials that exist
   validExamples <- list.files(system.file("tutorials", package = "trainr"))
 
   validExamplesMsg <-
     paste0(
-      cli::rule(center = crayon::red(" List of Tutorials ")), "\n",
-      crayon::blue(paste0("- ", validExamples, collapse = "\n")))
+      rule(center = red(" List of Tutorials ")), "\n",
+      blue(paste0("- ", validExamples, collapse = "\n")))
 
-  # if an invalid example is given, throw an error
-  if (missing(example) || !nzchar(example) ||
-      !example %in% validExamples) {
-    message(validExamplesMsg)
-  }
+  message(validExamplesMsg)
 
 }
